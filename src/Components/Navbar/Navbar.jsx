54a04c9033b8d0f181860ebf5408596854deb25a
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2026-02-05 04:38:20 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import MovieSearch from "../Home/MovieSearch";
 const Navbar = ({ setSearchTerm }) => {
   return (
     <div className="flex justify-between items-center py-4 pl-8 pr-32 bg-gray-800 text-white fixed top-0 w-full ">
-      <div className="text-2xl font-bold hidden sm:block">PopCorn Picks</div>
+      <div className="text-2xl font-bold hidden sm:block">PopCorn Six</div>
       <MovieSearch setSearchTerm={setSearchTerm} />
     </div>
   );
